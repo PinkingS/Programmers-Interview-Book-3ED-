c350048bd920a30f7474627553568ed9a00ac813
PinkingS <405768386@qq.com>
2014-01-23 10:26:21 +0800

    add 5-1

---
+++ Section2-CC++Programming/chapter5/5-1.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+
+int i = 1;
+void main(){
+	int i = i;
+	printf("The i in main is : %d\n", i);
+
+}
---
+++ Section2-CC++Programming/chapter5/5-1.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+
+int i = 1;
+int main(){
+	int i = i;
+	std::cout << "The i in main is : " << i << std::endl;
+	return 0;
+}
