7e7376a6e8bafaedf814b57c73c3191c4758f355
PinkingS <405768386@qq.com>
2014-02-17 16:01:42 +0800

    update

--- Section2-CC++Programming/chapter7/test.c
+++ Section2-CC++Programming/chapter7/test.c
@@ -1,11 +1,14 @@
 #include <stdio.h>
-
+#include <string.h>
 
 int main(){
-	char *c = "abcd";
+	char *c = "abcdefd";
 	//char a[] = "hello";
 	//c = a;
 	//printf("%c\n",*(c+1));
+	
+	printf("%d\n",sizeof(c));
+	printf("%d\n",strlen(c));
 	int a[] = {1, 2, 3, 4, 5};
 	printf("%08x\n",&a);
 	int *ptr = (int*)(&a+1);
